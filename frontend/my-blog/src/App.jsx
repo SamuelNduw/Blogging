d97f537fd48d3f5c88dbb35f15e77697dcc2f494
Samuel Nduw <samuelwamankalnduw@gmail.com>
2024-07-22 22:38:53 +0200

    Initial commit, Frontend setup

---
+++ frontend/my-blog/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-3xl font-archivo text-center'>
+        Blogging
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/my-blog/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter':['inter'],
+        'archivo':['archivo'],
+        'source-sans-3': ['source sans 3']
+      },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+      },
+  plugins: [],
+}
+
