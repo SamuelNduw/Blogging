4aa1fe14733bba14eafc75290fbf4b0aa763ea16
Samuel Nduw <samuelwamankalnduw@gmail.com>
2024-07-23 13:14:34 +0200

    Connected backend API to frontend using axios. Implemented the map function, to map over the JSON data and render the BlogCard component for each blog.

--- frontend/my-blog/src/components/BlogPosts.jsx
+++ frontend/my-blog/src/components/BlogPosts.jsx
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BlogCard from './BlogCard';
+import axios from 'axios';
 
 const BlogPosts = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/blogging')
+        .then(response => {
+            setBlogs(response.data);
+            console.log(response.data);
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    }, [])
     return (
         <div className='w-full px-8 font-archivo'>
             <h1 className='text-center font-bold text-4xl p-10'>
                 Blogging
             </h1>
             <div className='container mx-auto grid sm:grid-cols-3 grid-cols-1 gap-4 justify-items-center'>
-                <BlogCard title="Hello World" 
-                body="This is the first blog post on the Django + React JS project." 
-                author='Samuel Nduw'/>
-                <BlogCard title="Hello World" 
-                body="This is the first blog post on the Django + React JS project." 
-                author='John Doe'/>
-                <BlogCard title="Understanding Serverless Architecture for Scalable Applications" 
-                body="Serverless architecture allows developers to build scalable applications without managing servers. This model abstracts server management, enabling automatic scaling, reduced operational costs, and improved deployment speed, fostering agile development practices." 
-                author='Captain Price'/>
-                <BlogCard title="The Rise of Edge Computing in Modern Web Development" 
-                body="Edge computing is revolutionizing web development by bringing computation closer to the data source. This reduces latency, enhances performance, and ensures real-time processing, making applications faster and more efficient." 
-                author='Pink Banana'/>
-                <BlogCard title='Exploring AI in Web Design' 
-                body='AI transforms web design with personalized experiences and automated processes.' 
-                author='Logical King'/>
-                <BlogCard title='Benefits of Microservices Architecture' 
-                body='Progressive Web Apps (PWAs) combine the best of web and mobile applications. They offer offline capabilities, push notifications, and app-like experiences, enhancing user engagement and accessibility across various devices.' 
-                author='Green Star'/>
-                <BlogCard title='The power of GraphQL' 
-                body='GraphQL optimizes data fetching with precise queries.' 
-                author='Big CEO'/>
+                {
+                    blogs.length ?
+                    blogs.map((blog, index) => (
+                        <BlogCard title={blog.title}
+                        body={blog.body}
+                        author={blog.author} key={index}/>
+                    )) :
+                    null
+                }
             </div>
         </div>
     );
